e78d5b9ef0cacab7504297c7cc61d50c0ba68c99
Polvere243 <danielercoli@gmail.com>
2024-03-01 20:30:07 +0100

    first of the second group done

--- homework.sql
+++ homework.sql
@@ -16,3 +16,13 @@ SELECT * FROM `degrees` WHERE `name` LIKE '% Magistrale %';
 SELECT COUNT(`id`) AS `total_departments` FROM `departments`;
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 SELECT * FROM `teachers` WHERE `phone` IS NULL;
+
+
+
+-- 1. Contare quanti iscritti ci sono stati ogni anno
+SELECT COUNT(`id`) AS `students_per_year` FROM `students` GROUP BY YEAR(`enrolment_date`);
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+-- 3. Calcolare la media dei voti di ogni appello d'esame
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
