604119f16823e41c6aca75b3690e4339d4372c04
Polvere243 <danielercoli@gmail.com>
2024-03-01 19:58:44 +0100

    seventh one done

--- homework.sql
+++ homework.sql
@@ -13,5 +13,5 @@ SELECT * FROM `exams` WHERE HOUR(`hour`) >= '14' AND `date` = '2020-06-20';
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 SELECT * FROM `degrees` WHERE `name` LIKE '% Magistrale %';
 -- 7. Da quanti dipartimenti è composta l'università? (12)
-
+SELECT COUNT(`id`) AS `total_departments` FROM `departments`;
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
