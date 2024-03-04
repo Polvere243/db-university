5d7a331954ee5a3750bb1e7c50318bcd13fc4864
Polvere243 <danielercoli@gmail.com>
2024-03-04 10:06:23 +0100

    applied correction

--- homework.sql
+++ homework.sql
@@ -15,7 +15,7 @@ SELECT * FROM `degrees` WHERE `name` LIKE '% Magistrale %';
 -- 7. Da quanti dipartimenti è composta l'università? (12)
 SELECT COUNT(`id`) AS `total_departments` FROM `departments`;
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
-SELECT * FROM `teachers` WHERE `phone` IS NULL;
+SELECT COUNT('phone') FROM `teachers`;
 
 
 
